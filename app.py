2accb17b60dee081624413d9511d1407c2363b4a
mashitara <tnknora@gmail.com>
2026-07-15 22:19:27 +0900

    家計簿アプリはじめの一歩

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session
+from flask import Flask, render_template, redirect, url_for, session, request
 import mysql.connector
 
 app = Flask(__name__)
@@ -7,8 +7,8 @@ Hathaway = ["継続して見せる", "ゲームも", "勉強も", "筋トレも"
 
 @app.route("/")
 def index():
-    if not in session:
-        return redirect(url_for("login"))
+    #if not in session:
+    #    return redirect(url_for("login"))
 
     conn = mysql.connector.connect(
         host = "localhost",
@@ -19,7 +19,7 @@ def index():
 
     cursor = conn.cursor()
 
-    sql = "SELECT * FROM exepenses"
+    sql = "SELECT * FROM expenses"
 
     cursor.execute(sql)
 
@@ -29,3 +29,29 @@ def index():
 
 
     return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add():
+    conn = mysql.connector.connect(
+        host = "localhost",
+        user = "ms",
+        db = "expense_tracker",
+        password = ""
+    )
+
+    cursor = conn.cursor()
+
+    title = request.form["title"]
+    amount = request.form["amount"]
+    category = request.form["category"]
+    
+    sql = "INSERT INTO expenses (title, amount, category, user_id) VALUES (%s, %s, %s, 1)"
+
+    cursor.execute(sql, (title, amount, category,))
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port=5000, debug=True)
